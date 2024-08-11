530b28b663518e4c21825053379e2f63f1bb7c8d
BobbyBao <381675273@qq.com>
2024-08-12 00:11:27 +0800

    sample

--- samples/samples.cpp
+++ samples/samples.cpp
@@ -10,18 +10,31 @@ using namespace myecs;
 
 Database db;
 
-class MoveObject : public TComponentManager<float, float, float> {
-
+struct Vec2 {
+    float x;
+    float y;
 };
 
+class MoveObject : public TComponentManager<Vec2, Vec2> {
+};
 
 int main()
 {
+    auto& moveManager = *db.getPtr<MoveObject>();
 
-	auto e = db.create();
-	db.addComSet<MoveObject>();
+    for (int i = 0; i < 100000; i++) {
+        auto e = db.create();
+        auto inst = moveManager.addComponent(e);
+        moveManager.elementAt<0>(inst) = { (float)i, (float)i };
+        moveManager.elementAt<1>(inst) = { 2.0f, 2.0f };
+    }
 
-
-
-	return 0;
+    for (auto i = moveManager.begin(); i != moveManager.end(); i++) {
+        auto& p = moveManager.elementAt<0>(i);
+        auto& v = moveManager.elementAt<1>(i);
+        p.x += v.x;
+        p.y += v.y;
+        //std::cout << "x:" << p.x << ", y:" << p.y << std::endl;
+    }
+    return 0;
 }
